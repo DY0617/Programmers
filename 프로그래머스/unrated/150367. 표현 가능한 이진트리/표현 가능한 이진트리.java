07eb1eb8a63c3711cb96a7853e449f76011ef5ab
dongyeop <79496557+DY0617@users.noreply.github.com>
2023-01-25 23:57:12 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 47.09 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,55 +1,58 @@
-class Solution {
-    public boolean[] target; //2진법으로 변환한 배열 (좌측 0 패딩 포함
-    public int result;
+import java.util.*;
 
-    //Root 부터 탐색
-    public void solve(int s, int e, boolean isEnd) {
-        int mid = (s + e) / 2;
-        boolean cur = target[mid];
-        //중간에 0이 나왔는데 현재 노드가 1이라면
-        if (isEnd && cur) {
-            result = 0;
+class Solution {
+    
+    static char[] arr;
+    static int possible;
+    
+    public int[] solution(long[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for(int a=0;a<numbers.length;a++){
+            long now=numbers[a];
+
+            
+            String binary=Long.toBinaryString(now);
+            
+            int j = 0;
+        	while((int)Math.pow(2, j)-1 < binary.length()) {
+        		j++;
+        	}
+            
+        	while((int)Math.pow(2, j)-1 != binary.length()) {
+        		binary = "0"+ binary;
+        	}
+
+            arr=new char[binary.length()];
+            
+            for(int i=0;i<arr.length;i++){
+                arr[i]=binary.charAt(i);
+            }
+            
+            possible=1;
+            dfs(0,arr.length-1);
+            
+            answer[a]=possible;
+        }
+        
+        return answer;
+    }
+    
+    
+    public static void dfs(int start,int end){
+        int mid=(start+end)/2;       
+        
+        if(arr[mid]=='0'&&(arr[(start+mid-1)/2]=='1'||arr[(mid+1+end)/2]=='1')){
+
+            possible=0;
             return;
         }
-        //마지막 노드가 아닐 경우, 계속 진행
-        if (s != e) {
-            solve(s, mid-1, !cur);
-            solve(mid+1, e, !cur);
+        if(mid-start>1){
+            dfs(start,mid-1);
+            dfs(mid+1,end);
         }
-    }
-
-    public int[] solution(long[] numbers) {
-        int[] res = new int[numbers.length];
-        for (int ind = 0; ind < numbers.length; ind++) {
-            result = 1;
-            long num = numbers[ind];
-            //2진법 변환한 target 배열 생성
-            //해당 수의 2진법 길이 계산
-            int len = (int)Math.floor(Math.log(num) / Math.log(2)) + 1;
-            //해당 수의 포화 이진트리 길이 계산
-            int exp = 1;
-            int treeLen = 0;
-            while(true) {
-                treeLen = (int)Math.pow(2, exp++) - 1;
-                if (treeLen >= len) break;
-            }
-
-            target = new boolean[treeLen];
-            int i = treeLen - 1;
-            while(true) {
-                long div = num / 2;
-                long mod = num % 2;
-                num = div;
-                target[i--] = (mod == 1);
-                if (div == 1) {
-                    target[i] = true;
-                    break;
-                } else if (div == 0)
-                    break;
-            }
-            solve(0, treeLen - 1, false);
-            res[ind] = result;
-        }
-        return res;
+        else
+            return;
+        
     }
 }
