37ca0bf2e84c9109c673f484f459438358745d7d
dongyeop <79496557+DY0617@users.noreply.github.com>
2023-02-17 21:28:50 +0900

    [level 2] Title: 후보키, Time: 3.23 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    List<String> candi = new ArrayList<>();
+
+    public int solution(String[][] relation) {
+        int answer = 0;
+
+        for (int i = 0; i < relation[0].length; i++) {
+            boolean[] visited = new boolean[relation[0].length];
+            dfs(visited, 0, 0, i + 1, relation);
+        }
+        answer = candi.size();
+        return answer;
+    }
+
+    public void dfs(boolean[] visited, int start, int depth, int end, String[][] relation) {
+        if (depth == end) {
+            List<Integer> list = new ArrayList<>();
+            String key = "";
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) {
+                    key += String.valueOf(i);
+                    list.add(i);
+                }
+            }
+
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < relation.length; i++) {
+                String s = "";
+                for (Integer j : list) {
+                    s += relation[i][j];
+                }
+
+                if (map.containsKey(s)) {
+                    return;
+                } else {
+                    map.put(s, 0);
+                }
+            }
+
+            // 후보키 추가
+            for (String s : candi) { 
+                int count = 0;
+                for(int i = 0; i < key.length(); i++){
+                    String subS = String.valueOf(key.charAt(i));
+                    if(s.contains(subS)) count++;
+                }
+                if (count == s.length()) return;
+            }
+            candi.add(key);
+
+            return;
+        }
+
+        for (int i = start; i < visited.length; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            dfs(visited, i, depth + 1, end, relation);
+            visited[i] = false;
+        }
+
+    }
+}
