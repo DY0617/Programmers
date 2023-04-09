c7890728a0bd6dfd92c2d7815194e0d185e05bf4
dongyeop <79496557+DY0617@users.noreply.github.com>
2023-04-09 23:31:42 +0900

    [level 3] Title: 110 옮기기, Time: 72.76 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/lv3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/lv3/77886. 110 옮기기/110 옮기기.java
@@ -1,69 +1,30 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] s) {
-        String[] answer = new String[s.length];
-        
-        for(int i=0; i<s.length; i++)
-        {
-            int index = 0;
-            String temp = s[i];
-            int count = 0;
-            int sp = 0;
-            char[] stack = new char[temp.length()];
-            while(true)
-            {
-                if(sp >= 3)
-                {
-                    int end = sp-1;
-                    if(stack[end] == '0'&&stack[end-1] == '1'&&stack[end-2] == '1')
-                    {
-                        sp -=3;
-                        count++;
-                    }
-                    else if(index >= temp.length()) break;
-                    else stack[sp++]=temp.charAt(index++);
-                }
-                else if(index >= temp.length()) break;
-                else stack[sp++]=temp.charAt(index++);
-            }
-            temp = new String(stack).substring(0, sp);
-
-            //System.out.println(temp + " " + count);
-            StringBuilder appended = new StringBuilder();
-            for(int j=0; j<count; j++)
-                appended.append("110");
-            if(temp.length() == 0)
-            {
-                answer[i] = appended.toString();
-            }
-
-            int point = -1;
-            for(int j=0; j<temp.length()-1; j++)
-            {
-                if(temp.charAt(j)=='1' && temp.charAt(j+1)=='1')
-                {
-                    point = j;
-                    break;
-                }
-            }
-            if(point == -1)
-            {
-                point = 0;
-                for(int j=0; j<temp.length(); j++)
-                    if(temp.charAt(j)=='0')
-                        point = j+1;
-            }
-
-
-
-            //System.out.println(point);  
-
-            temp  = point == temp.length() ? temp + appended : temp.substring(0, point) + appended + temp.substring(point);
-            answer[i] = temp;
-
+        String[] ret = new String[s.length];
+        for(int i=0 ; i<s.length ; i++) {
+        	ret[i] = helper(s[i]);
         }
-        
-        return answer;
+        return ret;
     }
+    public String helper(String s) {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder plus = new StringBuilder();
+		
+		for(int i=0 ; i<s.length() ; i++) {
+			char c = s.charAt(i);
+			sb.append(c);
+			if(sb.length()>=3 && sb.charAt(sb.length()-3)=='1' && sb.charAt(sb.length()-2)=='1'  && sb.charAt(sb.length()-1)=='0') {
+				plus.append("110");
+				sb.delete(sb.length()-3, sb.length());
+			}
+		}
+		if(plus.length()>0) {
+			if(sb.indexOf("0")==-1) {
+				sb.insert(0,plus);
+			} else {
+				sb.insert(sb.lastIndexOf("0")+1,plus);
+			}
+		}
+		return sb.toString();
+	}
 }
