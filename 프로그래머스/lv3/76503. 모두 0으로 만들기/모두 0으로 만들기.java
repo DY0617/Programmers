728361a7ff457a75bb5cbdf9f0ca58d2164201c2
dongyeop <79496557+DY0617@users.noreply.github.com>
2023-04-10 17:02:33 +0900

    Update 모두 0으로 만들기.java

--- 프로그래머스/lv3/76503. 모두 0으로 만들기/모두 0으로 만들기.java
+++ 프로그래머스/lv3/76503. 모두 0으로 만들기/모두 0으로 만들기.java
@@ -1,50 +1,52 @@
 import java.util.*;
 
 class Solution {
-
-        long answer;
-        boolean[] visited;
-        long[] long_a;
-        ArrayList<Integer>[] children;
-
-        public long solution(int[] a, int[][] edges) {
-            this.answer = 0;
-            this.visited = new boolean[a.length];
-            this.children = new ArrayList[a.length];
-            this.long_a = new long[a.length];
-
-            long sum = 0;
-            for(int i = 0; i < a.length; i++){
-                sum += a[i];
-                children[i] = new ArrayList<>();
-                long_a[i] = a[i];
-            }
-
-            if(sum != 0) return -1;
-
-            for(int i = 0 ; i < edges.length; i++){
-                children[edges[i][0]].add(edges[i][1]);
-                children[edges[i][1]].add(edges[i][0]);
-            }
-
-            dfs(0);
-
-            return answer;
+    
+    long answer;
+    boolean[] visited;
+    long[] along;
+    
+    ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+    public long solution(int[] a, int[][] edges) {
+        answer=0;
+        along=new long[a.length];
+        visited=new boolean[a.length];
+        
+        long sum=0;
+        
+        for(int i=0;i<a.length;i++){
+            graph.add(new ArrayList<Integer>());
+            sum+=a[i];
+            along[i]=a[i];
         }
-
-        public long dfs(int v){
-            this.visited[v] = true;
-            
-            for(int i = 0; i < children[v].size(); i++){
-                int next = children[v].get(i);
-                if(!visited[next]){
-                    long_a[v] += dfs(next);
-                }
-            }
-
-            this.answer += Math.abs(long_a[v]);
-
-            return long_a[v];
+        
+        if(sum != 0) return -1;
+        
+        for(int i=0;i<edges.length;i++){
+            int from=edges[i][0];
+            int to=edges[i][1];
+            graph.get(from).add(to);
+            graph.get(to).add(from);
         }
-
+        
+        visited[0]=true;
+        dfs(0);
+        
+        return answer;
     }
+    
+    public long dfs(int loc){
+        
+        for(int i=0;i<graph.get(loc).size();i++){
+            int next=graph.get(loc).get(i);
+            if(!visited[next]){
+                visited[next]=true;
+                along[loc]+=dfs(next);
+            }
+        }
+        
+        answer+=Math.abs(along[loc]);
+        
+        return along[loc];
+    }
+}
